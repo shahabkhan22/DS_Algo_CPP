c374603467b0a970f802e9ae714b3f7bf8a1d748
shahabkhan22 <shahab2205@gmail.com>
2020-08-05 17:10:41 +0530

    ArrayList Class

--- ArrayList.cpp
+++ ArrayList.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
 
 class ArrayList(){
-    int arr[]
+    int *arr;
+    int num_elements;
+    int capacity;
+
+
+public:
+    ArrayList(int size){
+        arr = new int[size];
+        num_elements = 0;
+        capacity = size;
+    }
+
+    void insert(int val){
+        if(num_elements < capacity){
+            arr[num_elements] = val;
+            num_elements++;
+        }
+        else{
+            resize();
+            arr[num_elements] = val;
+            num_elements++;
+        }
+    }
+
+    int getAt(int index){
+        return arr[index];
+    }
+
+    
 };
